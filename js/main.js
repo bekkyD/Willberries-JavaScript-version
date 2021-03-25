72be68aa744b4810803c0a0673f282d4613ab8da
Bekky D <72444159+bekkyD@users.noreply.github.com>
2021-03-25 19:07:16 +0200

    3rd

--- js/main.js
+++ js/main.js
@@ -8,28 +8,131 @@ new Swiper('.swiper-container', {
 	},
 });
 // cart
-
 const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
+const viewAll = document.querySelectorAll('.view-all');
+const navigationLink = document.querySelectorAll('.navigation-link:not(.view-all)');
+const longGoodsList = document.querySelector('.long-goods-list');
+const showAccessories = document.querySelectorAll('.show-accessories');
+const showClothing = document.querySelectorAll('.show-clothing');
+const cartTableGoods = document.querySelector('.cart-table__goods');
+const cardTableTotal = document.querySelector('.card-table__total');
 
-const openModal = function() {
-	modalCart.classList.add('show')
+const getGoods = async () => {					//получаем массив со всеми товарами
+	const result = await fetch('db/db.json');	//получаем фич, который будет искать массив
+	if (!result.ok) {								//если с результатом что-то не в порядке
+		throw 'Ошибочка вышла:' + result.status		//выдаёт ошибку
+	}
+	return await result.json();						//а если всё в порядке - возвращает нам результат
+}													//await - тормозит функцию, выдаёт не процесс, а результат
+const cart = {
+	cartGoods: [],
+	renderCart(){
+		cartTableGoods.textContent = '';
+		this.cartGoods.forEach(({ id, name, price, count }) => {
+			const trGood = document.createElement('tr');
+			trGood.className = 'cart-item';
+			trGood.dataset.id = id;
+
+			trGood.innerHTML = `
+				<td>${name}</td>
+ 				<td>${price}</td>
+ 				<td><button class="cart-btn-minus">-</button></td>
+ 				<td>${count}</td>
+ 				<td><button class="cart-btn-plus">+</button></td>
+ 				<td>${price * count}$</td>
+ 				<td><button class="cart-btn-delete">x</button></td>
+			`;
+			cartTableGoods.append(trGood);
+		});
+
+		const totalPrice = this.cartGoods.reduce((sum, item) => {
+			return sum + item.price * item.count;
+		}, 0);
+
+		cardTableTotal.textContent = totalPrice + '$';
+	},
+	deleteGood(id) {
+		this.cartGoods = this.cartGoods.filter(item => id !== item.id);
+		this.renderCart();
+	},
+	minusGood(id) {
+		for (const item of this.cartGoods) {
+			if (item.id === id) {
+				if (item.count <= 1) {
+					this.deleteGood(id)
+				} else {
+					item.count--;
+				}
+				break;
+			}
+		}
+		this.renderCart();
+	},
+	plusGood(id) {
+		for (const item of this.cartGoods) {
+			if (item.id === id) {
+				item.count++;
+				break;
+			}
+		}
+		this.renderCart();
+	},
+	addCartGoods(id){
+		const goodItem = this.cartGoods.find(item => item.id === id);
+		if (goodItem) {
+			this.plusGood(id);
+		} else {
+			getGoods()
+				.then(data => data.find(item => item.id === id))
+				.then( ({ id, name, price }) => {
+					this.cartGoods.push({
+						id,
+						name,
+						price,
+						count: 1
+					});
+				});
+		}
+	},
 };
-const closeModal = function() {
+document.body.addEventListener('click', event => {
+	const addToCart = event.target.closest('.add-to-cart');
+	if (addToCart) {
+		cart.addCartGoods(addToCart.dataset.id)
+	}
+})
+cartTableGoods.addEventListener('click', event => {
+	const target = event.target;
+	if (target.classList.contains('cart-btn-delete')) {
+		const id = target.closest('.cart-item').dataset.id;
+		cart.deleteGood(id);
+	}
+	if (target.classList.contains('cart-btn-minus')) {
+		const id = target.closest('.cart-item').dataset.id;
+		cart.minusGood(id);
+	}
+	if (target.classList.contains('cart-btn-plus')) {
+		const id = target.closest('.cart-item').dataset.id;
+		cart.plusGood(id);
+	}
+});
+const openModal = () => {
+	cart.renderCart();
+	modalCart.classList.add('show');
+};
+const closeModal = () => {
 	modalCart.classList.remove('show');
 };
-
 buttonCart.addEventListener('click', openModal);
-modalCart.addEventListener('click', function(event) {
+modalCart.addEventListener('click', event => {
 	const target = event.target;
 
 	if (target.classList.contains('overlay') || target.classList.contains('modal-close')) {
 		closeModal();
 	}
 })
-
 //scroll smooth
-
 {
 	const scrollLinks = document.querySelectorAll('a.scroll-link');
 // цикл
@@ -45,7 +148,7 @@ modalCart.addEventListener('click', function(event) {
 // 	}
 	//другой вариант цикла - for of
 	for (const scrollLink of scrollLinks) {
-		scrollLink.addEventListener('click', function (event) {
+		scrollLink.addEventListener('click', event => {
 			event.preventDefault(); 													//отмена стандартного браузерного поведения
 			const id = scrollLink.getAttribute('href');
 			document.querySelector(id).scrollIntoView({ 							//а здесь мы добавляем объект
@@ -55,25 +158,8 @@ modalCart.addEventListener('click', function(event) {
 		});
 	}
 }
-
 // goods
-
-//получаем объекты из вёрстки
-
-const viewAll = document.querySelectorAll('.view-all');
-const navigationLink = document.querySelectorAll('.navigation-link:not(.view-all)');
-const longGoodsList = document.querySelector('.long-goods-list');
-const showAccessories = document.querySelectorAll('.show-accessories');
-const showClothing = document.querySelectorAll('.show-clothing');
-
 //получение данных
-const getGoods = async function() {					//получаем массив со всеми товарами
-	const result = await fetch('db/db.json');	//получаем фич, который будет искать массив
-	if (!result.ok) {								//если с результатом что-то не в порядке
-		throw 'Ошибочка вышла:' + result.status		//выдаёт ошибку
-	}
-	return await result.json();						//а если всё в порядке - возвращает нам результат
-}													//await - тормозит функцию, выдаёт не процесс, а результат
 getGoods().then(function (data) {					//then - даёт следующую команду, которая будет выполнена сразу после предыдущей
 	console.log(data)								//вывели данные в консоль
 });
@@ -99,7 +185,6 @@ const createCard = function ({ label, name, img, price, description, id }) {		//
 	`;
 	return card;
 };
-
 //выведение карточки на веб-страницу
 const renderCards = function(data) {
 	longGoodsList.textContent = ''; 								//очищение, удаление всего уже имеющегося контента на странице, быстро и безболезненно
@@ -112,40 +197,30 @@ const renderCards = function(data) {
 	longGoodsList.append(...cards)						//добавляет карточки
 	document.body.classList.add('show-goods')
 };
-
 const showAll = function(event) {
 	event.preventDefault();
 	getGoods().then(renderCards);
 }
-
-viewAll.forEach(function(elem) {
-	elem.addEventListener('click', function(event) {
+viewAll.forEach(elem => {
+	elem.addEventListener('click', event => {
 		event.preventDefault();
 		getGoods().then(renderCards);
 	});
 });
-
 //фильтрование объектов
 const filterCards = function(field, value) {
 	getGoods()
-		.then(function(data) {
-			const filteredGoods = data.filter(function(good) {
-				return good[field] === value
-			});
-			return filteredGoods;
-		})
+		.then(data => data.filter(good => good[field] === value))
 		.then(renderCards);
 };
-
-navigationLink.forEach(function(link) {
-	link.addEventListener('click', function(event) {
+navigationLink.forEach(link => {
+	link.addEventListener('click', event => {
 		event.preventDefault();
 		const field = link.dataset.field;
 		const value = link.textContent;
 		filterCards(field, value);
 	})
 });
-
 showAccessories.forEach(item => {
 	item.addEventListener('click', event => {
 		event.preventDefault();
